2d086068abc5aceb166d7dee8fb7d8770632f1fe
Victor Ungureanu <victor.ungureanu@rocketmail.com>
2013-12-06 01:55:12 +0200

    inclus packages si ce mai lipsea

---
+++ CocoFarm/CocoFarm.DataAccess/IDataStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CocoFarm.DataAccess
+{
+    public interface IDataStore<T>
+        where T : IEntityWithId
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        T Create(T entity);
+        T Update(T entity);
+        void Delete(int id);
+    }
+}
