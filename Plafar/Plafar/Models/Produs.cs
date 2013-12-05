d8d17403386aa2871f775700b9eb14b9f6a98984
Victor Ungureanu <victor.ungureanu@rocketmail.com>
2013-12-05 13:52:25 +0200

    schimbat titlu ecrane

--- Plafar/Plafar/Models/Produs.cs
+++ Plafar/Plafar/Models/Produs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace Plafar.Models
     [Table("Produs")]
     public class Produs
     {
+        [Key]
         public int Id { get; set; }
         
         public string Cod { get; set; }
